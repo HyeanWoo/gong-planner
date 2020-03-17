f31c82ba6995a64d9f70daa281ac347216fc33ef
SsangWoo <tkddn204@gmail.com>
2020-03-17 22:46:25 +0900

    firebase 추가

---
+++ src/firebase/index.js
@@ -0,0 +1,7 @@
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+// Initialize Firebase
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Firebase from './firebase';
+import firebaseConfig from "./firebase/firebase.json";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render( < App / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
