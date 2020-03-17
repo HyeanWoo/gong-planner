0cf3425e455f6f12725e455a14ce74d9715f8923
SsangWoo <tkddn204@gmail.com>
2020-03-17 22:57:27 +0900

    index.js 수정 - 파이어베이스 관련

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Firebase from './firebase';
+import {
+    firebase
+} from './firebase';
 import firebaseConfig from "./firebase/firebase.json";
 import * as serviceWorker from './serviceWorker';
 
-Firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 
