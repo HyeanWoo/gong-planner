077640b2b61ae01d3979de505bbc1bc54f61d379
unknown <wkdus7h@naver.com>
2020-03-18 21:00:18 +0900

    get data from firestore

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
+import firebase from '../firebase'
 
 class Subjects extends Component {
   state = {
-    subjects : [
-      {id: 1, subjectName: '생물', totalElapsedTime: '05:00:03'},
-      {id: 2, subjectName: '물리', totalElapsedTime: '03:40:55'},
-      {id: 3, subjectName: '화학', totalElapsedTime: '01:20:12'}
-    ],
-
+    subjects : [ {id: 1, subjectName: '생물', totalElapsedTime: '05:00:03'},
+    {id: 2, subjectName: '물리', totalElapsedTime: '03:40:55'},
+    {id: 3, subjectName: '화학', totalElapsedTime: '01:20:12'}],
     tmpSubjectName: '',
   }
 
   componentDidMount() {
-    // firebase
+    // this.getData();
   }
 
   handleChange = (e) => {
@@ -34,9 +32,10 @@ class Subjects extends Component {
     });
   }
 
-  // updateSubject = (id) => {
-  //   let changed = 
-  // }
+  updateSubject = (id) => {
+    let changed = this.state.subjects.find( subject => subject.id === id);
+    console.log(changed);
+  }
 
   deleteSubject = (id) => {
     let subjects = this.state.subjects.filter( subject => subject.id !== id);
@@ -50,6 +49,22 @@ class Subjects extends Component {
   //   처음에는 +버튼으로 표시하고 누르면 input창이 나타나면서 과목추가하게 만들기
   // }
 
+  getData = () => {
+    const db = firebase.firestore();
+    let subRef = db.collection("testSubject");
+
+    subRef.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.setState({
+          subjects : [...this.state.subjects, doc.data()]
+        });
+      });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+  }
+
   render() {
     const { subjects } = this.state;
     const subjectList = subjects.length ? (
@@ -58,6 +73,7 @@ class Subjects extends Component {
           <div className="subject-card" key={subject.id}>
             <div>{subject.subjectName}</div>
             <div>{subject.totalElapsedTime}</div>
+            <button onClick={()=>{this.updateSubject(subject.id)}}>수정</button>
             <button onClick={()=>{this.deleteSubject(subject.id)}}>삭제</button>
           </div>
         )
@@ -70,8 +86,8 @@ class Subjects extends Component {
 
     return(
       <div className="container col s5 offset-s2 orange subjects">
+          <button onClick={this.createSubject}>+</button>
         <form onSubmit={this.addSubject}>
-          {/* <button onClick={this.createSubject}>+</button> */}
           <input className="orange lighten-1" type="text" id="tmpSubjectName" value={this.state.tmpSubjectName}
             placeholder="과목을 입력해보세요!" onChange={this.handleChange}/>
         </form>
--- src/components/Todaylog.js
+++ src/components/Todaylog.js
@@ -4,7 +4,8 @@ const Todaylog = () => {
   return(
     <div className="row todaylog">
       <div className="col s5 offset-s2 green one-word">
-        오늘의 한마디
+        <div>오늘의 한마디</div>
+        <div>- 열공 ^^7 -</div>
       </div>
       <div className="col s3 blue d-day">
         <div>D-day</div> 
