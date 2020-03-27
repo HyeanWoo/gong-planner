336422689776ef3f8157514d5cf4a03f2f87c6fe
SsangWoo <tkddn204@gmail.com>
2020-03-27 20:21:52 +0900

    날짜 선택에 pickers 적용

--- src/components/TodayDate.js
+++ src/components/TodayDate.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DayjsUtils from '@date-io/dayjs';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
@@ -11,7 +13,16 @@ const TodayDate = ({ date, onChangeDate }) => {
 	return (
 		<React.Fragment>
 			<ArrowBackIosIcon className="prevDate" onClick={onClickArrowBack} />
-			{date.format(dateFormat)}
+			<MuiPickersUtilsProvider utils={DayjsUtils}>
+				<KeyboardDatePicker
+					format="YYYY. MM. DD."
+					value={date.format(dateFormat)}
+					onChange={onChangeDate}
+					KeyboardButtonProps={{
+						'aria-label': 'change date',
+					}}
+				/>
+			</MuiPickersUtilsProvider>
 			<ArrowForwardIosIcon className="nextDate" onClick={onClickArrowForward} />
 		</React.Fragment>
 	);
